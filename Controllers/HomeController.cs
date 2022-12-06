8788157c8cc61be0736a3b72b13d48f1eb51a8de
meghnakundur <110425939+meghnakundur@users.noreply.github.com>
2022-12-06 15:16:41 -0600

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kundur_Meghna_HW1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        //TODO: Create a new action method for the new page
+        public IActionResult Professional()
+        {
+            return View();
+        }
+
+        public IActionResult Resume()
+        {
+            string path = _environment.WebRootPath + "/files/Resume_Meghna Kundur1.pdf";
+            var stream = new FileStream(path, FileMode.Open);
+            return File(stream, "application/pdf", "Resume_Meghna Kundur1.pdf");
+        }
+    }
+}
